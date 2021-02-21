a5c954bdfcc5a31f0fbd4fe8bacba6bac2be8293
FionaAu911 <56212188+FionaAu911@users.noreply.github.com>
2021-02-20 22:04:18 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,7 +15,6 @@ public class Main {
         // System.out.println("Choose your opponent: \n 1. An agent that use MINIMAX \n 2. An agent that use MINIMAX with Alpha-beta pruning \n 3. An agent that uses H-MINIMAX with a fixed depth cutoff and a-b pruning \n Your choice?");
         // int opponent = scan.nextInt();
         /* each choice lead to a different opponent */
-
         System.out.println("Do you want to play DARK (x) or LIGHT (o)?");
         // char player = scan.next().charAt(0);
         char player = 'x'; //for testing
@@ -23,5 +22,7 @@ public class Main {
         /* prints board */
         Board b = new Board(size, player);
         b.printBoard(b.gameBoard);
+
+        scan.close();
     }
 }
