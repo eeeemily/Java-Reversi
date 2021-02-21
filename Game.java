74bcf501cef37a0b1ab166d7c0a6525c10f7a16b
Emily Zheng <mzheng9@u.rochester.edu>
2021-02-21 02:51:03 -0500

    1.added Actions which should return a list of possible moves, still need to debug it tho. 2.renamed Board to State

--- Game.java
+++ Game.java
@@ -1,49 +1,47 @@
 public class Game {
-/* initialize state, makes a board, decides who's turn is it, call the algorithms
-implement these class?
-MINIMAX
-MINIMAX with alpha beta..
-H-MINIMAX with alpha beta..
-UTILITY (decides who wins)
- */
- 
-    int minimax(Board b) {
-        //count number of x's and o's to see if player wins at the end
+    /*
+     * initialize state, makes a State, decides who's turn is it, call the
+     * algorithms implement these class? MINIMAX MINIMAX with alpha beta.. H-MINIMAX
+     * with alpha beta.. UTILITY (decides who wins)
+     */
+
+    int minimax(State b) {
+        // count number of x's and o's to see if player wins at the end
         if (terminalTest(b) == true) {
             return utility(b);
         } else if (b.aiTurn == true) { // if it is the ai's turn return max
-            
+
         } else { // human's turn return min
 
         }
         return 0;
     }
 
-    boolean terminalTest(Board b) {
+    boolean terminalTest(State b) {
         // also test for if both players can do legal moves
-        for (int i = 0; i<b.size; i++) { //get < size
-            for (int j =0; j<b.size; j++){
-                if (b.gameBoard[i][j] != '-') { // see if whole board is x's and o's
+        for (int i = 0; i < b.size; i++) { // get < size
+            for (int j = 0; j < b.size; j++) {
+                if (b.gameState[i][j] != '-') { // see if whole State is x's and o's
                     return false;
                 }
             }
         }
         return true;
     }
- 
-    int utility(Board b) {
+
+    int utility(State b) {
         int xCount = 0;
         int oCount = 0;
-        for (int i = 0; i<b.size; i++) {
-            for (int j =0; j<b.size; j++){
-                if (b.gameBoard[i][j] == 'x') {
+        for (int i = 0; i < b.size; i++) {
+            for (int j = 0; j < b.size; j++) {
+                if (b.gameState[i][j] == 'x') {
                     xCount++;
                 } else {
                     oCount++;
                 }
             }
         }
-        if(b.getPlayer() == 'o') { // if ai is x, then if there are more x's on board, ai has won, so return 1
+        if (b.getPlayer() == 'o') { // if ai is x, then if there are more x's on State, ai has won, so return 1
             if (xCount > oCount) {
                 return 1;
             } else if (xCount < oCount) {
--- Main.java
+++ Main.java
@@ -1,27 +1,31 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Choose your game: \n 1. Small 4x4 Reversi \n 2. Standard 8x8 Reversi");
         int boardChoice = scan.nextInt();
         int size;
-        if (boardChoice == 1){
+        if (boardChoice == 1) {
             size = 4;
-        }else {
+        } else {
             size = 8;
         }
-        
-        // System.out.println("Choose your opponent: \n 1. An agent that use MINIMAX \n 2. An agent that use MINIMAX with Alpha-beta pruning \n 3. An agent that uses H-MINIMAX with a fixed depth cutoff and a-b pruning \n Your choice?");
+
+        // System.out.println("Choose your opponent: \n 1. An agent that use MINIMAX \n
+        // 2. An agent that use MINIMAX with Alpha-beta pruning \n 3. An agent that uses
+        // H-MINIMAX with a fixed depth cutoff and a-b pruning \n Your choice?");
         // int opponent = scan.nextInt();
         /* each choice lead to a different opponent */
         System.out.println("Do you want to play DARK (x) or LIGHT (o)?");
         // char player = scan.next().charAt(0);
-        char player = 'x'; //for testing
-        
+        char player = 'x'; // for testing
+
         /* prints board */
-        Board b = new Board(size, player);
-        b.printBoard(b.gameBoard);
+        State b = new State(size, player);
+        b.printState(b.gameState);
+        Actions posibleMoves = new Actions(b, 'x');
+        posibleMoves.printActions();
 
         scan.close();
     }
